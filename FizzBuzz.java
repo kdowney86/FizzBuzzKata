76a9a348e200d26e2fd4e5f5f912b3e1ff443f8e
Kelvin <kelvin.downey@oramon.nl>
2017-05-15 10:36:54 +0200

    Default setup

---
+++ FizzBuzz.java
@@ -0,0 +1,5 @@
+/**
+ * Created by kelvin on 15/05/17.
+ */
+public class FizzBuzz {
+}
---
+++ FizzBuzzTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+/**
+ * Created by kelvin on 15/05/17.
+ */
+public class FizzBuzzTest {
+
+
+}
