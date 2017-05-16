02da339e066dc0d5b3f3025c555214b53c95ccf4
Kelvin <kelvin.downey@oramon.nl>
2017-05-16 09:47:37 +0200

    Kata - Refactor functionality for numbers divisible by 5

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,10 +4,11 @@
 public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
 
     public String getString(int number) {
         if (number % 3 == 0) return FIZZ;
-        if (number % 5 == 0) return "Buzz";
+        if (number % 5 == 0) return BUZZ;
         return null;
     }
 
