32ae0246b5f6821dd38ceb914a8ccc712559ec33
Kelvin <kelvin.downey@oramon.nl>
2017-05-17 10:03:55 +0200

    Kata - Refactor functionality for numbers divisible by or containing 3

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -6,13 +6,22 @@ public class FizzBuzz {
     public static final String FIZZ = "fizz";
     public static final String BUZZ = "buzz";
 
+    public static final int FIZZ_NUMBER = 3;
+    public static final int BUZZ_NUMBER = 5;
+
     public String getString(int number) {
         String result = "";
-        if (number % 3 == 0 || Integer.toString(number).contains("3")) result += FIZZ;
-        if (number % 5 == 0) result += BUZZ;
+        if (containsOrIsDivisibleBy(number, FIZZ_NUMBER)) result += FIZZ;
+        if (number % BUZZ_NUMBER == 0) result += BUZZ;
 
         if (result.length() == 0) result = Integer.toString(number);
         return result;
+    }
+
+    private boolean containsOrIsDivisibleBy(int number, int testNumber) {
+        String numberString = Integer.toString(number);
+        String testNumberString = Integer.toString(testNumber);
+        return number % testNumber == 0 || numberString.contains(testNumberString);
     }
 
 }
