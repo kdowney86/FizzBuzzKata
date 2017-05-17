e0c53ee868f62c29ec914b25218427f5d87b7e08
Kelvin <kelvin.downey@oramon.nl>
2017-05-17 09:43:16 +0200

    Kata - Add test for numbers divisible by three

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -1,8 +1,17 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 /**
  * Created by kelvin on 15/05/17.
  */
 public class FizzBuzzTest {
 
+    FizzBuzz fizzBuzz = new FizzBuzz();
 
+    final String FIZZ = "fizz";
+
+    @Test
+    public void testDivByThree() {
+        String result = fizzBuzz.getString(3);
+        Assert.assertEquals(FIZZ, result);
+    }
 }
