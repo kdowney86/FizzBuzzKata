39d4443e5cb00e563ab9df7ee78eeded4f04871d
Kelvin <kelvin.downey@oramon.nl>
2017-05-17 09:57:50 +0200

    Kata - Add functionality that returns Fizz when numbers contain 3

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -8,7 +8,7 @@ public class FizzBuzz {
 
     public String getString(int number) {
         String result = "";
-        if (number % 3 == 0) result += FIZZ;
+        if (number % 3 == 0 || Integer.toString(number).contains("3")) result += FIZZ;
         if (number % 5 == 0) result += BUZZ;
 
         if (result.length() == 0) result = Integer.toString(number);
