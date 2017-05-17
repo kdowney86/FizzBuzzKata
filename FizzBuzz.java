05085bd37ca521b591c6ddb02eda5c8af4e0c120
Kelvin <kelvin.downey@oramon.nl>
2017-05-17 09:49:52 +0200

    Kata - Add functionality for numbers divisible by 3 and 5

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -8,8 +8,10 @@ public class FizzBuzz {
 
     public String getString(int number) {
 
+        if (number % 3 == 0 && number % 5 ==0) return FIZZ + BUZZ;
         if (number % 3 == 0) return FIZZ;
         if (number % 5 == 0) return BUZZ;
+
 
         return null;
     }
