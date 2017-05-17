48b9d498b79578be72be343eb0bbb76d40c6e2e3
Kelvin <kelvin.downey@oramon.nl>
2017-05-17 09:45:51 +0200

    Kata - Add test for numbers divisible by 5

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -8,10 +8,17 @@ public class FizzBuzzTest {
     FizzBuzz fizzBuzz = new FizzBuzz();
 
     final String FIZZ = "fizz";
+    final String BUZZ = "buzz";
 
     @Test
     public void testDivByThree() {
         String result = fizzBuzz.getString(3);
         Assert.assertEquals(FIZZ, result);
+    }
+
+    @Test
+    public void testDivByFive() {
+        String result = fizzBuzz.getString(5);
+        Assert.assertEquals(BUZZ, result);
     }
 }
