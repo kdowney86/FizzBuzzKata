f154f476edcf74389e6c294c02683d9e6bb0c3d9
Kelvin <kelvin.downey@oramon.nl>
2017-05-18 09:43:33 +0200

    Kata - Add test for numbers divisible by 3

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,4 +2,8 @@
  * Created by kelvin on 15/05/17.
  */
 public class FizzBuzz {
+
+    public String getString(int number) {
+        return null;
+    }
 }
--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -1,8 +1,23 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 /**
  * Created by kelvin on 15/05/17.
  */
 public class FizzBuzzTest {
 
+    FizzBuzz fizzBuzz = new FizzBuzz();
 
+    public static final String FIZZ = "fizz";
+
+    @Test
+    public void testReturnFizzWhenDivisibleBy3() {
+        //given
+        int testNumber = 3;
+
+        //when
+        String result = fizzBuzz.getString(testNumber);
+
+        //then
+        Assert.assertEquals(FIZZ, result);
+    }
 }
