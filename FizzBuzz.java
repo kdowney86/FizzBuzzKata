f8501f6feee018505cf5112ea0efd3274dd6ccac
Kelvin <kelvin.downey@oramon.nl>
2017-05-18 09:48:26 +0200

    Kata - Refactor div by 3 or 5 functionality

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,9 +4,14 @@
 public class FizzBuzz {
 
     public static final String FIZZ = "fizz";
+    public static final String BUZZ = "buzz";
 
     public String getString(int number) {
-        if (number % 5 == 0) return "buzz";
-        return FIZZ;
+        String result = "";
+        if (number % 3 == 0) result += FIZZ;
+        if (number % 5 == 0) result += BUZZ;
+
+        return result;
+
     }
 }
