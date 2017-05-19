a3cf0bf73345d340b26c1135d59d7abb8faa34f9
Kelvin <kelvin.downey@oramon.nl>
2017-05-19 08:22:32 +0200

    Kata - Refactor functionality for fizz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,4 +2,14 @@
  * Created by kelvin on 15/05/17.
  */
 public class FizzBuzz {
+
+    public static final String FIZZ = "fizz";
+
+    public String getString(int number) {
+        String result = "";
+
+        if (number % 3 == 0) result = FIZZ;
+
+        return result;
+    }
 }
