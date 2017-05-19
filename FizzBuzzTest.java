5c5bece8806f7c070939b93b8c5b1b818bb9ee46
Kelvin <kelvin.downey@oramon.nl>
2017-05-19 08:18:44 +0200

    Kata - Add failing test for 3 returning fizz

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -1,8 +1,31 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 /**
  * Created by kelvin on 15/05/17.
  */
 public class FizzBuzzTest {
 
+    public static final String FIZZ = "fizz";
+
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    private void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+
+    @Test
+    public void testThreeReturnsFizz() {
+        //given
+        int testNumber = 3;
+
+        //when
+        String result = fizzBuzz.getString(testNumber);
+
+        //then
+        Assert.assertEquals(FIZZ, result);
+    }
 
 }
